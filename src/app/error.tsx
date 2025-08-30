960310570c8143bf894ef9160297a978ee648e72
Noone <kasraam385@gmail.com>
2025-08-31 02:14:03 +0430

    do something idk why

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,6 +1,6 @@
 "use client"; // Error boundaries must be Client Components
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { AlertTriangle, RotateCcw } from "lucide-react";
 
 export default function Error({
@@ -10,12 +10,15 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
+  const [err, setErr] = useState<Error | undefined>(undefined);
+
   useEffect(() => {
-    console.error(error); // Log error
+    console.error(error);
+    setErr(error);
   }, [error]);
 
   return (
-    <div className="flex flex-col items-center justify-center h-[calc(100vh-20vh)] px-4">
+    <div className="flex flex-col items-center justify-center h-screen px-4">
       <div className="bg-white dark:bg-[#1c1c1c] shadow-xl flex flex-col rounded-2xl p-8 max-w-md text-center">
         <div className="flex justify-center text-red-500 mb-4">
           <AlertTriangle className="w-12 h-12" />
@@ -23,9 +26,12 @@ export default function Error({
         <h2 className="text-2xl font-semibold mb-2 text-gray-900 dark:text-white">
           Oops! Something went wrong.
         </h2>
-        <p className="text-gray-600 dark:text-gray-300 mb-6">
-          We encountered an unexpected error. Please try again or contact
-          support if the issue persists.
+        <p className="text-gray-600 dark:text-gray-300">
+          We encountered an unexpected error. Please try again if the issue
+          persists.
+        </p>
+        <p className="text-gray-600 font-medium dark:text-gray-300 mb-6">
+          The Error: {err?.message ?? ""}
         </p>
         <button
           onClick={() => reset()}
